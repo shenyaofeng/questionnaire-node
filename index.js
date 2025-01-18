cc7186d6e532a296eed0c6d10552bbfe5bc1bbb3
shenyaofeng <1051406614@qq.com>
2025-01-18 12:15:15 +0800

    模拟发送数据

---
+++ index.js
@@ -0,0 +1,27 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const mockList = require('./mock/index');
+const app = new Koa();
+const router = new Router();
+
+async function getRes(fn) {
+  return new Promise(resolve=>{
+    setTimeout(()=>{
+      const res = fn();
+      resolve(res);
+    },1000)
+  })
+}
+
+// 注册mock路由
+mockList.forEach(item=>{
+  const {url,method,response} = item;
+  router[item.method](url,async ctx=>{
+    const res = await getRes(response);
+    ctx.body = res; //输出
+  })
+})
+
+
+app.use(router.routes());
+app.listen(3000)
